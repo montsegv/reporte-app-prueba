ae630033f4e182dcca861780c79501c64b03dca3
Montserrat <maguilar14@ucol.mx>
2020-10-07 22:17:54 -0500

    diseño pagina signup

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -7,45 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  avatars = [
-    {
-      img: 'av-1.png',
-      seleccionado: true
-    },
-    {
-      img: 'av-2.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-3.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-4.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-5.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-6.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-7.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-8.png',
-      seleccionado: false
-    },
-];
-
-  avatarSlide = {
-    slidesPerView: 3.5
-  }
-
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/auth/signup/signup.page.ts
+++ src/app/auth/signup/signup.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IonSlide } from '@ionic/angular';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
+  @ViewChild('slidePrincipal') slides: IonSlide;
+
+  avatars = [
+    {
+      img: 'av-1.png',
+      seleccionado: true
+    },
+    {
+      img: 'av-2.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-3.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-4.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-5.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-6.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-7.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-8.png',
+      seleccionado: false
+    },
+];
+
+  avatarSlide = {
+    slidesPerView: 3.5
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  login( fLogin: NgForm ) {
+      console.log(fLogin.valid);
+  }
+
+  registro( fRegistro: NgForm ) {
+      console.log( fRegistro.valid );
+  }
+
+  seleccionarAvatar( avatar ){
+    this.avatars.forEach( av => av.seleccionado = false );
+    avatar.seleccionado = true;
+  }
+
 
 }
